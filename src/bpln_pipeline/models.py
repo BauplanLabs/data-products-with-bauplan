a88dfbf1658b124c3eadbee17f2861753c081186
Jacopo Tagliabue <tagliabue.jacopo@gmail.com>
2025-02-12 16:44:22 -0500

    add model

--- src/bpln_pipeline/models.py
+++ src/bpln_pipeline/models.py
@@ -1,37 +1,61 @@
 import bauplan
 
 
-@bauplan.python('3.10', pip={'polars': '1.19.0'})
-@bauplan.model(materialization_strategy='REPLACE')
-def extract_customer_id_from_logs(
-    customer_logs=bauplan.Model(
-      'ddog.datadog_logs',
+# bauplan decorator for the Python function
+@bauplan.python('3.11', pip={'pandas': '2.2.0'})
+@bauplan.model(
+  # since this is a demo, make the table statetless and replace it everytime
+  # to simplify running the demo on your own - 'APPEND' instead of 'REPLACE'
+  # could be used in a real-world scenario
+  materialization_strategy='REPLACE',
+  # this name matches the name of the output port
+  name='amountStatsTable',
+  # this is the schema of the output port, both as a reference
+  # and for potential checks in bauplan
+  columns=[
+    'tripDate',
+    'avgTotal',
+    'avgTip',
+    'avgTolls',
+  ]
+)
+def calculate_trip_stats(
+    trips=bauplan.Model(
+      'tripsTable',
+      # this both serves as a reference to the input port and as a way to
+      # push down the column selection to object storage for maximum performance
       columns=[
-        'message',
-        'request_id',
-      ],
-      filter="event_timestamp > $start_date AND type='customer' AND function_name=$funct_name"
+        'tpep_pickup_datetime',
+        'Tip_amount', 
+        'Tolls_amount',
+        'Total_amount'
+      ]
     )
 ):
     """
 
-    Given a set of logs of type 'customer', extract the customer id from the free-form logging
-    and return a table mapping the request id to the customer id, for joining with other tables
-    downstream:
+    Given a set of rows, each representing a specific trip in a date, calculate the average of the numerical columns,
+    and rename the date column to match the output port schema. The final table will be persisted in the system, 
+    and will have the following schema (one row per date):
     
-    | request_id | customer_id |
-    |------------|-------------|
-    | 123        | 456         |
+    | tripDate     | avgTotal | avgTip | avgTolls |
+    |------------- |----------|--------|----------|
+    | "2020-01-01" | 12.3     | 1.2    | 0.1      |
 
     """
-    import pyarrow as pa
-    print(f"\n\n===> Number of customer logs retrieved: {customer_logs.num_rows}\n\n")
-    # process the logs and extract the customer id
-    # e.g. 'message': '######## 8 ########' > 8
-    messages = customer_logs['message'].to_pylist()
-    customer_ids = [ int(_.split(' ')[1]) for _ in messages ]
-    customer_logs = customer_logs.append_column('customer_id', pa.array(customer_ids))
-    # return the table with the request id and the customer id
-    return customer_logs.select(['request_id', 'customer_id'])
+    import pandas as pd
+    print(f"\n\n===> Number of trips retrieved: {trips.num_rows}\n\n")
+    df = trips.to_pandas()
+    # calculate the average of the numerical columns
+    result = df.groupby("date_as_string", as_index=False).agg(
+        avgTip=("Tip_amount", "mean"),
+        avgTolls=("Tolls_amount", "mean"),
+        avgTotal=("Total_amount", "mean"),
+    )
+    # rename the date column to match the output port schema
+    result.rename(columns={"tpep_pickup_datetime": "tripDate"}, inplace=True)
+    
+    # return the table to the system for persistence
+    return result
 
 
